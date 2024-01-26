d56132d73480f7bd17f122e03652accc3e13af46
amarubale10 <amarubale85@gmail.com>
2024-01-26 22:30:52 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,167 @@
+export const Bio = {
+  name: "Amar  Ubale",
+  roles: "Full Stack Developer",
+
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/amarubale10",
+  resume:
+    "https://drive.google.com/file/d/1obFiO89yDTCrNfhz31XzwtdfVsyxF3TD/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/amar-ubale",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+
+      {
+        name: "SpringBoot",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/spring/spring-original.svg",
+      },
+      {
+        name: "CSharp",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/csharp/csharp-original.svg",
+      },
+
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cdac.in/img/C-DAC.jpg",
+    school: "Centre for Development of Advanced Computing ",
+    date: "march 2023 - Sep 2023",
+    percentage: "70.13 %",
+    degree: "PG-DAC (Post Graduation Diploma in Advanced Computing)",
+  },
+  {
+    id: 1,
+    img: "https://images.shiksha.com/mediadata/images/1619429005phpyoTPKv.jpeg",
+    school: "G H Raisoni College of Engineering & Management, Pune",
+    date: "2018 - 2021",
+    percentage: "61.70 %",
+    degree: "B-Tech",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "E-Pathology",
+    date: "Jun 2023 - Sep 2023",
+    description:
+      " E-Pathology Lab Management System is a platform, that allows customers to book a pathology test in a hassle free way and provide conclusive reports.",
+    image:
+      "https://www.frigorifix.com/wp-content/uploads/2022/07/Tests-are-included-in-the-Pathology-Lab.jpg",
+    tags: ["SpringBoot", "React Js", "MySql", "Axios", "Node Js", "Redux"],
+
+    github: "https://github.com/amarubale10/E-Pathology",
+  },
+
+  {
+    id: 10,
+    title: "Sahyadri Quest",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "A captivating online platform dedicated to sahyadri Trekking, Camping and Workation experiences.",
+    image: "https://static.toiimg.com/photo/msid-99604289.cms",
+    tags: ["HTML", "JavaScript", "CSS"],
+
+    github: "https://github.com/amarubale10/Sahyadri-Quest",
+  },
+];
